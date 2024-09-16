a66bacbe1f6b1ee1a62e7ce9a1d1934fa01ee3f7
Dat Nguyen <dat.nguyen@sleek.com>
2024-09-16 20:02:01 +0700

    Just a little bit of styling

--- src/app/components/JsonTable.tsx
+++ src/app/components/JsonTable.tsx
@@ -22,19 +22,19 @@ export default function JsonTable({ data }: JsonTableProps) {
       )
     );
     return (
-      <Table>
+      <Table className="border-collapse border border-black w-auto h-auto">
         <TableHeader>
-          <TableRow>
+          <TableRow className="bg-gray-200">
             {headers.map((header) => (
-              <TableHead key={header}>{header}</TableHead>
+              <TableHead key={header} className="border border-black p-0.5 text-xs font-bold">{header}</TableHead>
             ))}
           </TableRow>
         </TableHeader>
         <TableBody>
           {data.map((item, index) => (
-            <TableRow key={index}>
+            <TableRow key={index} className={index % 2 === 0 ? "bg-white" : "bg-gray-100"}>
               {headers.map((header) => (
-                <TableCell key={header}>
+                <TableCell key={header} className="border border-black p-0.5 text-xs">
                   {typeof item === "object" ? (
                     <JsonTable data={item[header]} />
                   ) : header === "" ? (
@@ -51,12 +51,12 @@ export default function JsonTable({ data }: JsonTableProps) {
     );
   } else if (typeof data === "object" && data !== null) {
     return (
-      <Table>
+      <Table className="border-collapse border border-black w-auto h-auto">
         <TableBody>
-          {Object.entries(data).map(([key, value]) => (
-            <TableRow key={key}>
-              <TableCell className="font-medium">{key}</TableCell>
-              <TableCell>
+          {Object.entries(data).map(([key, value], index) => (
+            <TableRow key={key} className={index % 2 === 0 ? "bg-white" : "bg-gray-100"}>
+              <TableCell className="font-bold border border-black p-0.5 text-xs">{key}</TableCell>
+              <TableCell className="border border-black p-0.5 text-xs">
                 <JsonTable data={value} />
               </TableCell>
             </TableRow>
@@ -65,6 +65,6 @@ export default function JsonTable({ data }: JsonTableProps) {
       </Table>
     );
   } else {
-    return <p>{data as React.ReactNode}</p>;
+    return <p className="text-xs">{data as React.ReactNode}</p>;
   }
 }
